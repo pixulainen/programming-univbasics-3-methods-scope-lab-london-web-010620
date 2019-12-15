eceb63f8ee26953bd838d6311feb09414132a000
George Apetrei <george.apetrei@gmail.com>
2019-12-15 14:48:24 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,11 +2,11 @@ $phrase = "It's-a me, Mario!"
 $status = "Thank You Mario! But Our Princess Is In Another Castle!"
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
 def link
 puts  'It\'s Dangerous To Go Alone! Take This.'
